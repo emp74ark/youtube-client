cdddc04c6193c11f79aab64f06daa0dbe5c716fd
emp74ark <emp74ark@gmail.com>
2022-12-28 18:11:50 +0100

    feat: add 404 page

---
+++ src/app/components/ufo/ufo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ufo',
+  templateUrl: './ufo.component.html',
+  styleUrls: ['./ufo.component.scss']
+})
+export class UfoComponent {
+
+}
